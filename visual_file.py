ba55086dab5d50040af8e046023a56e563008740
stefimaz <89653430+stefimaz@users.noreply.github.com>
2021-11-06 13:24:23 -0400

    last update

--- visual_file.py
+++ visual_file.py
@@ -15,18 +15,10 @@ from scipy.stats import norm
 import statistics
 
 
-
-header = st.container()
-dataset = st.container()
-features = st.container()
-model_training = st.container()
-
-
-
 st.title('Dividends Reinvestment Dashboard')
 st.text('Analysis of the power of Dividend Reinvestment.')
 st.text('This project will let you select one stock that pays dividends.')
-st.text('Once chosen, will be able to choose a few different options.')
+st.text('Once chosen, you will be able to select a few different options.')
 
 
     
@@ -35,29 +27,117 @@ crypto = ("BTC-USD", "ETH-USD")
 options = ("Same Stock", "Crypto", "Keep the cash")
 
 dropdown_stocks = st.selectbox('Pick your stock', tickers)
-#for close in tickers():
-#    st.write('The curent value of this stock is:', i)  
-    
-start = st.date_input('Start Date', value = pd.to_datetime('2011-01-01'))
-end = st.date_input('End Date', value = pd.to_datetime('today'))
+     
+start = st.date_input('Start Date', value= pd.to_datetime('2011-01-01'))
+end = st.date_input('End Date', value= pd.to_datetime('today'))
 
-@st.cache
-def relativeret(df):
-    rel = df.pct_change()
-    cumret = (1 + rel).cumprod() - 1
-    cumret = cumret.fillna(0)
-    return cumret
+#@st.cache
+def close_price():
+    data = yf.download(dropdown_stocks)['Adj Close']
+    start_Date = pd.to_datetime('today')
+    end_Date = pd.to_datetime('today')
+    timeframe = (start_date, end_date)
+    price = (data, timeframe)
+    
+    return price
+
+    
 if len(dropdown_stocks) > 0:
-    df = relativeret(yf.download(dropdown_stocks, start, end)['Adj Close'])
-    st.header('Returns of {}'.format(dropdown_stocks))
-    st.line_chart(df)
-        
+    df2 = yf.download(dropdown_stocks, start, end)['Adj Close']
+    st.header('Historical value of {}'.format(dropdown_stocks))
+    st.text('The current value is ${}'.format(close_price))
+    st.line_chart(df2)
+
+#def relativeret(df):
+#    rel = df.pct_change()
+#    cumret = (1 + rel).cumprod() - 1
+#    cumret = cumret.fillna(0)
+#    return cumret
+#if len(dropdown_stocks) > 0:
+#    df = relativeret(yf.download(dropdown_stocks, start, end)['Adj Close'])
+#    st.header('Cumulative returns of {}'.format(dropdown_stocks))
+#    st.line_chart(df)
+#if len(dropdown_stocks) > 0:
+#    df3 = yf.download(dropdown_stocks, start, end)['dividendYield']['dividendRate']
+#    st.bar_chart(df3)
+    
 #share_amount= st.slider('How many shares do you want?', min_value=10, max_value=500, value=20, step=5)
 share_amount= st.number_input('How many shares do you want?', min_value=1)   
-
+#def total_price()
  
-dropdown_option = st.multiselect('Where do you want to reinvest your dividends?', options)
-dropdown_crypto = st.multiselect('What crypto would you like to reinvest in?', crypto)
+dropdown_option = st.selectbox('Where do you want to reinvest your dividends?', options)
+#@st.cache
+#if options == Same Stock
+#    def stock(tickers):
+
+dropdown_crypto = st.selectbox('What crypto would you like to reinvest in?', crypto)
+
+
+
+####### I believe all code should be inserted here using the if, elif , else method
+        
+        
+dropdown_stocks_dividend = st.selectbox('Pick your stock dividend', tickers) 
+
+#@st.cache
+#def dividend(df2):
+#    rel = df2.pct_change()
+#    cumret = (1 + rel).cumprod() - 1
+#    cumret = cumret.fillna(0)
+#    return cumret
+#if len(dropdown_stocks) > 0:
+#    df2 = yf.download(dropdown_stocks, start, end)['dividendRate']
+#    df2 = dividend(yf.download(dropdown_stocks, start, end)['dividendYield'])
+#    st.header('Returns of {}'.format(dropdown_stocks))
+#    st.bar_chart(df2)
+
+#def sip(investment, tenure, interest, amount=0, is_year=True, is_percent=True, show_amount_list=False):
+#    tenure = tenure*12 if is_year else tenure
+#    interest = interest/100 if is_percent else interest
+#    interest /= 12
+#    amount_every_month = {}
+#    for month in range(tenure):
+#        amount = (amount + investment)*(1+interest)
+#        amount_every_month[month+1] = amount
+#    return {'Amount after invexting your dividends back into the chosen stock': amount, 'Amount every month': amount_every_month} #if show_amount_list else {'Amount after investing your dividends back into the chosen stock': amount} 
+#years = input()
+# (monthly amount, years, percent returned)
+
+#SIP_maturity = sip(monthly_f_div, 20, average_annual_return["F"])
+
+#print(SIP_maturity)
+
+
+
+
 dropdown_div = st.multiselect('This stock will return', tickers)
 
+
+
+
+
+
+#returns = last_price.pct_change().dropna()
+# number of simulations
+#number_simulations = 1000
+#number_days = 1400
+
+#simulation_df = pd.DataFrame()    
+#for x in range(number_simulations):
+#    count = 0
+#    daily_volatility= returns.std()
+    
+#    price_series = []
+    
+#    for y in range(number_days):
+#        if count == 1400:
+#            break
+#            price = price_series[count] * (1 + np.random.normal(0, daily_volatility))
+#            price_series.append(price)
+#            count += 1
+            
+#        simulation_df[x]= price_series
+        
+#simulation_df.head()
+    
     
